051d8f6e8a403c65313c5102457943b212abcacf
Daniel Wagner <me@dmwit.com>
2021-03-13 01:24:09 -0500

    delete the segment view
    We're calculating this on the Haskell side now (in finishableSegments)
    because it's so much faster. The view is no longer needed, and deleting
    it makes it more likely that I catch myself trying to use it in the
    future and remember that I should use finishableSegments instead.

--- src/schema.sql
+++ src/schema.sql
@@ -15,15 +15,4 @@ create table split (
 	primary key (run, seq_no)
 );
 
-create view segment as (
-	select run.game, run.fps, run.started, f.run, f.seq_no,
-	       f.state as f_state, t.state as t_state,
-	       t.microsecond - f.microsecond as duration,
-	       f.microsecond as f_microsecond, t.microsecond as t_microsecond,
-	       f.frame as f_frame, t.frame as t_frame
-	from split as f
-	join run on f.run = run.id
-	join split t on f.run = t.run and f.seq_no + 1 = t.seq_no
-);
-
 /* TODO: indexes */
